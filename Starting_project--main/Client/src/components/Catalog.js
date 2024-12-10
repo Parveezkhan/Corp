19fe34e59d0ee16acc72654bf3c9ac331377c20a
Parveez Khan <parveezkhan9611@gmail.com>
2024-12-10 19:56:36 +0530

    changes

--- Starting_project--main/Client/src/components/Catalog.js
+++ Starting_project--main/Client/src/components/Catalog.js
@@ -25,17 +25,20 @@ const Catalog = () => {
             Dashboard > Courses > Amazon Web > Services > Account-Lab
           </div>
           <div className="nav-settings d-flex ">
-            <div className="configurations border border-2 mx-2  text-center">
-             <img src={ec2} alt="" style={{width:'80px' , height:"80px"}} className="rounded-circle "></img>
-              <h3>Configurations for EC2</h3>
+            <div className="configurations border border-2 mx-2  text-start">
+             <img src={ec2} alt="" style={{ height:"150px"}} className=" w-100"></img>
+             <div className="p-2">
+             <h3>Configurations for EC2</h3>
               <p><span>EC2 Instance</span>: tb.micro</p>
               <p><span>Operating System</span>: Windows </p>
               <p><span>Ram</span>: 4 GiB</p>
               <p><span>Storage</span>: 100 GB</p>
+             </div>
+              
             </div>
           <div className="lab-details justify-content-center  p-3  border border-2 d-flex justify-content-between mx-2">
             {/* <div className="icon"><GrSystem /></div> */}
-            <div className="align-items-center">
+            <div className="d-flex  flex-column align-items-center">
             <div className="title"><span className="m-2"><GrSystem /></span>Amazon Elastic Cloud Computing</div>
             <div className="start-btn text-center"><button className="btn btn-success m-2" style={{width:'100px'}}>Start</button></div>
 
